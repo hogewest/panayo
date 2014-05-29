9a397787e330b0d264dea5b9a1b75feae6d35f91
hogewest <hogewest@gmail.com>
2014-05-29 22:20:59 +0900

    Refactor say in panayo.rb

--- lib/panayo.rb
+++ lib/panayo.rb
@@ -2,14 +2,12 @@ require "panayo/version"
 require "panayo/converter"
 
 module Panayo
-  class << self
-    def say(text)
-      return if text.nil?
+  def self.say(text)
+    return if text.nil?
 
-      words = Panayo::Converter.to_kana(text)
-      words = Panayo::Converter.remove_dakuten(words)
-      words = Panayo::Converter.remove_handakuten(words)
-      Panayo::Converter.to_hanayo(words)
-    end
+    words = Panayo::Converter.to_kana(text)
+    words = Panayo::Converter.remove_dakuten(words)
+    words = Panayo::Converter.remove_handakuten(words)
+    Panayo::Converter.to_hanayo(words)
   end
 end
