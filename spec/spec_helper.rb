68ce5a93bdd7120fba4fcab990ddc40d01fac627
hogewest <hogewest@gmail.com>
2014-08-21 22:38:24 +0900

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'panayo'
 require 'coveralls'
 Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'panayo'
